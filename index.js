ee16d993a3738ac7f03f3547d5756588e3c41cef
Tobi-Shit <78197272+Tobi-Shit@users.noreply.github.com>
2021-02-18 20:47:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
-const { create, Client } = require('@open-wa/wa-automate') // As consts aqui declaram as funções de outros arquivos
-const welcome = require('./lib/welcome') // Ou de modulos que usei
-const kconfig = require('./config')
-const options = require('./options')
-const color = require('./lib/color')
-const config = require('./lib/config/config.json')
+const  { create , Client }  =  require ( '@ open-wa / wa-automate' )  // As consts aqui declaram as funções de outros arquivos
+const  welcome  =  require ( './lib/welcome' )  // Ou de modulos que usei
+const  kconfig  =  require ( './config' )
+ opções  const =  require ( './options' )
+const  color  =  require ( './lib/color' )
+const  config  =  require ( './lib/config/config.json' )
 
-// Cria um cliente de inicialização da BOT
-const start = (kill = new Client()) => {
-    console.log(color('\n> DEV OFICIAL ='), color(' KillovSky > https://wa.me/+5518998044132', 'yellow'))
-	console.log(color('\n> GRUPO OFICIAL ='), color(' https://chat.whatsapp.com/H53MdwhtnRf7TGX1VJ2Jje', 'yellow'))
-	console.log(color('\n>'), color('Inicialização finalizada, os comandos podem ser usados agora...', 'red'))
+// Cria um cliente de inicialização do BOT
+const  start  =  ( kill =  new  Client ( ) )  =>  {
+    console . log ( cor ( '\ n> DEV OFICIAL =' ) ,  cor ( 'KillovSky> https://wa.me/+5531984159437' ,  'amarelo' ) )
+	console . log ( cor ( '\ n> GRUPO OFICIAL =' ) ,  cor ( 'https://chat.whatsapp.com/KCV4iZ2TrRm1D0whPyCmrv' ,  'amarelo' ) )
+	console . log ( color ( '\ n>' ) ,  color ( 'Inicialização finalizada, os comandos podem ser usados ​​agora ...' ,  'vermelho' ) )
 	
-		// Forçar recarregamento caso obtenha erros
-		kill.onStateChanged((state) => {
-			console.log('[Estado da Íris]', state)
-			if (state === 'UNPAIRED' || state === 'CONFLICT' || state === 'UNLAUNCHED') kill.forceRefocus()
-		})
+		// Forçar recarregamento caso erros erros
+		matar . onStateChanged ( ( state )  =>  {
+			console . log ( '[Estado da Íris]' ,  estado )
+			if  ( state  ===  'UNPAIRED'  ||  state  ===  'CONFLICT'  ||  state  ===  'UNLAUNCHED' )  kill . forceRefocus ( )
+		} )
 	
 		
         // Le as mensagens e limpa cache
-        kill.onMessage((async (message) => {
-            kill.getAmountOfLoadedMessages()
-            .then((msg) => {
-                if (msg >= 2000) {
-                    kill.cutMsgCache()
+        matar . onMessage ( ( assíncrono  ( mensagem )  =>  {
+            matar . getAmountOfLoadedMessages ( )
+            . então ( ( msg )  =>  {
+                if  ( msg > = 2000 )  {
+                    matar . cutMsgCache ( )
                 }
-            })
-            kconfig(kill, message)
-        }))
+            } )
+            kconfig ( matar ,  mensagem )
+        } ) )
 		
 		// Configuração do welcome
-        kill.onGlobalParicipantsChanged(async (heuh) => {
-            await welcome(kill, heuh)
-        })
+        matar . onGlobalParicipantsChanged ( async  ( heuh )  =>  {
+            aguardar  bem-vindo ( matar ,  heuh )
+        } )
         
 		
-		// Funções para caso seja adicionada em um grupo
-        kill.onAddedToGroup(async (chat) => {
-			const wlcmsg = 'Oi! 🌟\nFui requisitada como BOT para esse grupo, e estarei a disposição de vocês! 🤖\nSe quiserem ver minhas funcões usem /menu!'
-			const lmtgru = await kill.getAllGroups()
-            let totalMem = chat.groupMetadata.participants.length
-			if (chat.groupMetadata.participants.includes(config.owner)) {
-				await kill.sendText(chat.id, wlcmsg)
-			} else if (gc.length > config.memberLimit) {
-            	await kill.sendText(chat.id, `Um novo grupo, Eba! 😃\nUma pena que vocês não tem o requisito, que é ter pelo menos ${config.memberLimit} membros. Você possui ${totalMem}, junte mais pessoas! 😉`)
-				await kill.leaveGroup(chat.id)
-				await kill.deleteChat(chat.id)
-			} else if (lmtgruc.length > config.gpLimit) {
-				await kill.sendText(chat.id, `Desculpe, estamos no maximo de grupos!\nAtualmente estamos em ${lmtgru.length}/${config.gpLimit}`)
-				await kill.leaveGroup(chat.id)
-				await kill.deleteChat(chat.id)
-            } else {
-                kill.sendText(chat.id, wlcmsg)
+		// Funções para caso seja adicionado em um grupo
+        matar . onAddedToGroup ( async  ( chat )  =>  {
+			const  wlcmsg  =  'Oi! 🌟 \ nFui requisitado como BOT para esse grupo, e estarei a disposição de vocês! 🤖 \ nSe quiserem ver minhas funcões usem / menu! '
+			const  lmtgru  =  Await  mortes . getAllGroups ( )
+            deixe  totalMem  =  conversar . groupMetadata . participantes . comprimento
+			if  ( chat . groupMetadata . participantes . inclui ( config . proprietário ) )  {
+				aguarde a  morte . sendText ( chat . id ,  wlcmsg )
+			}  else  if  ( gc . length  >  config . memberLimit )  {
+            	aguarde a  morte . sendText ( chat . id ,  `Um novo grupo, Eba! 😃 \ nUma pena que vocês não tem o requisito, que é ter pelo menos $ { config . memberLimit } membros. Você possui $ { totalMem } , junte mais pessoas! 😉` )
+				aguarde a  morte . leaveGroup ( chat . id )
+				aguarde a  morte . deleteChat ( chat . id )
+			}  else  if  ( lmtgruc . length  >  config . gpLimit )  {
+				aguarde a  morte . sendText ( chat . id ,  `Desculpe, estamos no maximo de grupos! \ nAtualmente estamos em $ { lmtgru . length } / $ { config . gpLimit } ` )
+				aguarde a  morte . leaveGroup ( chat . id )
+				aguarde a  morte . deleteChat ( chat . id )
+            }  else  {
+                matar . sendText ( chat . id ,  wlcmsg )
             }
-        })
+        } )
 		
 
         // Bloqueia na call
-        kill.onIncomingCall(async (call) => {
-            await kill.sendText(call.peerJid, `Que pena! Chamadas não são suportadas e atrapalham muito! 😊\nTe bloqueei para evitar novas, contate o dono wa.me/${config.owner.replace('c.us', '')} para efetuar o desbloqueio. 👋`)
-            await kill.contactBlock(call.peerJid)
-        })
+        matar . onIncomingCall ( assíncrono  ( chamada )  =>  {
+            aguarde a  morte . sendText ( call . peerJid ,  `Que pena! Chamadas não são suportadas e atrapalham muito! 😊 \ nTe bloqueei para evitar novas, contate o dono wa.me/ $ { config . owner . replace ( 'c.us' ,  '' ) } para efetuar o desbloqueio. 👋` )
+            aguarde a  morte . contactBlock ( call . peerJid )
+        } )
     }
 
-create(options(true, start))
-    .then((kill) => start(kill))
-    .catch((err) => new Error(err))
+criar ( opções ( verdadeiro ,  iniciar ) )
+    . então ( ( matar )  =>  iniciar ( matar ) )
+    . catch ( ( err )  =>  novo  erro ( err ) )
